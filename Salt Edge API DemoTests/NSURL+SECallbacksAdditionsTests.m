4c556d26494bf6848060bbc6eabb169c5d572e27
Constantin Lungu <koctake@gmail.com>
2014-10-30 19:29:21 +0200

    Fix SECallbackAdditions spec due to renaming

--- Salt Edge API DemoTests/NSURL+SECallbacksAdditionsTests.m
+++ Salt Edge API DemoTests/NSURL+SECallbacksAdditionsTests.m
@@ -1,5 +1,5 @@
 //
-//  NSURL+SBCallbacksAdditionsTests.m
+//  NSURL+SECallbacksAdditionsTests.m
 //  SaltBridge Demo
 //
 //  Created by nemesis on 7/17/14.
@@ -7,13 +7,13 @@
 //
 
 #import <XCTest/XCTest.h>
-#import "NSURL+SBCallbacksAdditions.h"
+#import "NSURL+SECallbacksAdditions.h"
 
-@interface NSURL_SBCallbacksAdditionsTests : XCTestCase
+@interface NSURL_SECallbacksAdditionsTests : XCTestCase
 
 @end
 
-@implementation NSURL_SBCallbacksAdditionsTests
+@implementation NSURL_SECallbacksAdditionsTests
 
 static NSURL* homeURL, *emptyCallbackURL;
 
@@ -24,7 +24,7 @@ static NSURL* homeURL, *emptyCallbackURL;
     homeURL = [[NSURL alloc] initWithScheme:@"https" host:@"www.saltedge.com" path:@"/"];
 
     // A simple SaltBridge callback URL with no data
-    emptyCallbackURL = [[NSURL alloc] initWithScheme:SBCallbackScheme host:SBCallbackHost path:@"/"];
+    emptyCallbackURL = [[NSURL alloc] initWithScheme:SECallbackScheme host:SECallbackHost path:@"/"];
 }
 
 - (void)tearDown
@@ -34,39 +34,39 @@ static NSURL* homeURL, *emptyCallbackURL;
     [super tearDown];
 }
 
-- (void)testSb_isCallbackURL
+- (void)testSe_isCallbackURL
 {
-    NSURL* badHostCallbackURL   = [[NSURL alloc] initWithScheme:SBCallbackScheme host:@"www.saltedge.com" path:@"/"];
-    NSURL* badSchemeCallbackURL = [[NSURL alloc] initWithScheme:@"https" host:SBCallbackHost path:@"/"];
+    NSURL* badHostCallbackURL   = [[NSURL alloc] initWithScheme:SECallbackScheme host:@"www.saltedge.com" path:@"/"];
+    NSURL* badSchemeCallbackURL = [[NSURL alloc] initWithScheme:@"https" host:SECallbackHost path:@"/"];
 
-    XCTAssertTrue(emptyCallbackURL.sb_isCallbackURL, @"URL having the callback scheme should be a SaltBridge callback URL");
-    XCTAssertFalse(homeURL.sb_isCallbackURL, @"Usual URL without the callback scheme shouldn't be a SaltBridge callback URL");
-    XCTAssertFalse(badHostCallbackURL.sb_isCallbackURL, @"URL with host not being connect shouldn't be a SaltBridge callback URL");
-    XCTAssertFalse(badSchemeCallbackURL.sb_isCallbackURL, @"URL with scheme not being saltbridge shouldn't be a SaltBridge callback URL");
+    XCTAssertTrue(emptyCallbackURL.se_isCallbackURL, @"URL having the callback scheme should be a SaltBridge callback URL");
+    XCTAssertFalse(homeURL.se_isCallbackURL, @"Usual URL without the callback scheme shouldn't be a SaltBridge callback URL");
+    XCTAssertFalse(badHostCallbackURL.se_isCallbackURL, @"URL with host not being connect shouldn't be a SaltBridge callback URL");
+    XCTAssertFalse(badSchemeCallbackURL.se_isCallbackURL, @"URL with scheme not being saltbridge shouldn't be a SaltBridge callback URL");
 }
 
-- (void)testSb_callbackParameters
+- (void)testSe_callbackParameters
 {
     NSError* error = nil;
-    NSURL* successCallbackURL = [[NSURL alloc] initWithScheme:SBCallbackScheme host:SBCallbackHost path:@"/{\"data\":{\"login_id\":22, \"state\":\"success\"}}"];
-    NSURL* badCallbackURL = [[NSURL alloc] initWithScheme:SBCallbackScheme host:SBCallbackHost path:@"/Not a valid JSON string"];
+    NSURL* successCallbackURL = [[NSURL alloc] initWithScheme:SECallbackScheme host:SECallbackHost path:@"/{\"data\":{\"login_id\":22, \"state\":\"success\"}}"];
+    NSURL* badCallbackURL = [[NSURL alloc] initWithScheme:SECallbackScheme host:SECallbackHost path:@"/Not a valid JSON string"];
     NSDictionary* expectedCallbackParameters = @{ @"data" : @{ @"login_id": @22, @"state" : @"success" } };
-    BOOL successCallbacksEqual = [[successCallbackURL sb_callbackParametersWithError:&error] isEqualToDictionary:expectedCallbackParameters];
+    BOOL successCallbacksEqual = [[successCallbackURL se_callbackParametersWithError:&error] isEqualToDictionary:expectedCallbackParameters];
 
     // Succcess callback URL
     XCTAssertNil(error, @"Error should be nil due to success of successful callback parameters serialization");
     XCTAssertTrue(successCallbacksEqual, @"The parameters should be equal to those given in the path string");
 
     // Home URL
-    XCTAssertNil([homeURL sb_callbackParametersWithError:&error], @"Usual URL without the callback scheme should not have any callback parameters in it");
+    XCTAssertNil([homeURL se_callbackParametersWithError:&error], @"Usual URL without the callback scheme should not have any callback parameters in it");
     XCTAssertNil(error, @"Error should be nil due to the fact that the URL isn't a SaltBridge callback URL");
 
     // Empty callback URL
-    XCTAssertNil([emptyCallbackURL sb_callbackParametersWithError:&error], @"The empty callback URL should have an empty parameter list in it's path");
+    XCTAssertNil([emptyCallbackURL se_callbackParametersWithError:&error], @"The empty callback URL should have an empty parameter list in it's path");
     XCTAssertNil(error, @"Error should be empty due to the fact that callback parameters string is empty");
 
     // Bad callback URL
-    XCTAssertNil([badCallbackURL sb_callbackParametersWithError:&error], @"An invalid JSON string in the callback parameters should return nil");
+    XCTAssertNil([badCallbackURL se_callbackParametersWithError:&error], @"An invalid JSON string in the callback parameters should return nil");
     XCTAssertNotNil(error, @"Error shouldn't be nil due to the fact that the JSON string couldn't be serialized into an object");
     XCTAssertEqual(error.code, 3840, @"The error code should be a NSPropertyListReadCorruptError");
     XCTAssertEqual(error.domain, NSCocoaErrorDomain, @"Error's domain should be NSCocoaErrorDomain");
