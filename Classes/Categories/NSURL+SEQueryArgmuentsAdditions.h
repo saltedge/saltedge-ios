4ece3449625a861b23c2ef071d2b2699a6410367
Constantin Lungu <koctake@gmail.com>
2014-10-30 19:18:30 +0200

    Added a new category for fetching GET query params, renamed the old callback category

---
+++ Classes/Categories/NSURL+SEQueryArgmuentsAdditions.h
@@ -0,0 +1,13 @@
+//
+//  NSString+SEURLQueryArgmuentsAdditions.h
+//  Salt Edge API Demo
+//
+//  Created by nemesis on 10/30/14.
+//  Copyright (c) 2014 Salt Edge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (SEURLQueryArgmuentsAdditions)
+
+@end
---
+++ Classes/Categories/NSURL+SEQueryArgmuentsAdditions.m
@@ -0,0 +1,13 @@
+//
+//  NSString+SEURLQueryArgmuentsAdditions.m
+//  Salt Edge API Demo
+//
+//  Created by nemesis on 10/30/14.
+//  Copyright (c) 2014 Salt Edge. All rights reserved.
+//
+
+#import "NSString+SEURLQueryArgmuentsAdditions.h"
+
+@implementation NSString (SEURLQueryArgmuentsAdditions)
+
+@end
